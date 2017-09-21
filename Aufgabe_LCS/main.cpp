8cf4a20768423c713845258e37fbc5bd97caea29
mkugler <michel.kugler@hf-ict.info>
2017-09-21 12:17:51 +0200

    LCS V2

---
+++ Aufgabe_LCS/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "StringUtil.h"
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+void fillString(string &string1, int n) {
+    for (int i = 0; i < n; ++i) {
+        string1 += (char) ((rand() % 26) + 'A');
+    };
+    //string1 += "Hallo Welt, ich bin ein Test String";
+};
+
+void loadFromFileToString(string filePath, string &toFill) {
+    string temp;
+    ifstream file(filePath);
+    if (file.is_open()) {
+        while (file.good()) {
+            getline(file, temp);
+            toFill += temp;
+        }
+    }
+    file.close();
+}
+
+int main() {
+    time_t start, stop;
+
+    string testString1 = "";
+    string testString2 = "";
+
+    //Abraham Lincoln vs. Freimaurer (WIKI)
+
+    loadFromFileToString("../Audi.txt", testString1);
+    loadFromFileToString("../Tesla.txt", testString2);
+
+    //Putin vs. Trump (WIKI)
+    /*
+    loadFromFileToString("../DonaldTrump", testString1);
+    loadFromFileToString("../Putin.txt", testString2);
+    */
+
+    //Abraham Lincoln vs. Freimaurer (WIKI)
+    /*
+    loadFromFileToString("../AbrahamLincoln.txt", testString1);
+    loadFromFileToString("../Freimaurer.txt", testString2);
+    */
+
+    // Fill String with Random Numbers
+    /*
+    fillString(testString1, 10000);
+    fillString(testString2, 100000);
+    */
+
+
+    start = clock();
+    cout << "Längster String = " << StringUtil::lcs(testString1, testString2) << endl;
+    stop = clock();
+
+    cout << "String1 length => " << testString1.length() << endl;
+    cout << "String2 length => " << testString2.length() << endl;
+    cout << "Runtime => " << (double) (stop - start) / CLOCKS_PER_SEC << endl;
+
+    return 0;
+}
